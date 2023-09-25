a52459d73153172c3e7116af8991718e259d4a2a
Joao Valadares <joaopgvaladares@gmail.com>
2023-09-25 14:39:16 -0300

    Prepare to deploy

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+//https://eth-sepolia.g.alchemy.com/v2/d7hmqKAq1qPfUeFthU1M2UiNLwGigZDi
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+
+
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/d7hmqKAq1qPfUeFthU1M2UiNLwGigZDi",
+      accounts: ['7ed5152f06c66d6829fe70c2a0221dec98fe2f874e398e2af60f13041039c428']
+    }
+  }
 };
